9d546ed9c8e664fdacaed6b5c230b0e062f5ec0f
FhmiAksn <fahmiaksannugroho@gmail.com>
2024-12-18 14:00:22 +0700

    add reponsive class

--- src/Artikel/Page.tsx
+++ src/Artikel/Page.tsx
@@ -9,10 +9,10 @@ export default function ArtikelPage() {
       background="bg-white"
       padding="px-0 py-9"
       content={
-        <div className="">
+        <div className="space-y-6">
           {
             dataArtikel.map((item, index) => (
-              <div key={index} className="grid grid-cols-4 relative place-items-center">
+              <div key={index} className="md:grid flex flex-col md:grid-cols-4 relative justify-center md:place-items-center">
                 <img src={item.thumbnail} alt={item.content} />
                 <div className="col-span-3 bg-[#F2EE6F] rounded-tr-xl rounded-bl-xl p-4 ">
                   <Link to={`/artikel/${item.id}`} className="font-bold text-xl">{item.title}</Link>
